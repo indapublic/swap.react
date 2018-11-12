10e1064a86d2a7f8e57a48b52aa6b893ed716309
Vasilii Zolotukhin <mail@indapublic.com>
2018-11-12 21:56:20 +1000

    Fixed resources duplicates

--- shared/components/modals/OfferModal/AddOffer/AddOffer.js
+++ shared/components/modals/OfferModal/AddOffer/AddOffer.js
@@ -371,7 +371,7 @@ export default class AddOffer extends Component {
           <span styleName="error">
             <FormattedMessage id="transaction27" defaultMessage="For a swap, you need" />
             {minAmount.eth}
-            <FormattedMessage id="transaction27" defaultMessage="ETH on your balance" />
+            <FormattedMessage id="transaction28" defaultMessage="ETH on your balance" />
           </span>
         }
         <SelectGroup
